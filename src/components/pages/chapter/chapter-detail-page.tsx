ed0b74553df41049dc6489a220783048e43d601b
Aman Kori <amankori2307@gmail.com>
2025-01-27 23:57:14 +0530

    Refactored Code

--- src/components/pages/chapter/chapter-detail-page.tsx
+++ src/components/pages/chapter/chapter-detail-page.tsx
@@ -6,41 +6,46 @@ import ChapterContent from "../../sub-components/chapter/chapter-content";
 import ChapterHeader from "../../sub-components/chapter/chapter-header";
 
 const ChapterDetailsPage = () => {
-    const { chapterId } = useParams<{ chapterId: string }>();
-    const { chapter, vocabularyEntryList, loading, reloadChapter } = useChapterDetails(chapterId);
-    const [showAddVocabPopup, setShowAddVocabPopup] = useState(false);
+  const { chapterId } = useParams<{ chapterId: string }>();
+  const { chapter, vocabularyEntryList, loading, reloadChapter } =
+    useChapterDetails(chapterId);
+  const [showAddVocabPopup, setShowAddVocabPopup] = useState(false);
 
-    const togglePopup = () => {
-        setShowAddVocabPopup(!showAddVocabPopup);
-    };
+  const togglePopup = () => {
+    setShowAddVocabPopup(!showAddVocabPopup);
+  };
 
-    return (
-        <div className="p-6 bg-gray-100 min-h-screen">
-            <div className="max-w-4xl mx-auto">
-                {/* Chapter Header */}
-                {chapter && (
-                    <ChapterHeader
-                        chapter={chapter}
-                        onAddVocab={togglePopup}
-                        isLoading={loading}
-                    />
-                )}
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <div className="max-w-4xl mx-auto">
+        {/* Chapter Header */}
+        {chapter && (
+          <ChapterHeader
+            chapter={chapter}
+            onAddVocab={togglePopup}
+            isLoading={loading}
+          />
+        )}
 
-                {/* Chapter Content */}
-                <ChapterContent vocabularyEntryList={vocabularyEntryList} isLoading={loading} chapterId={chapterId as string} onRemoveVocabulary={reloadChapter} />
-            </div>
+        {/* Chapter Content */}
+        <ChapterContent
+          vocabularyEntryList={vocabularyEntryList}
+          isLoading={loading}
+          chapterId={chapterId as string}
+          onRemoveVocabulary={reloadChapter}
+        />
+      </div>
 
-            {/* Add Sentence Popup */}
-            {chapterId && showAddVocabPopup && (
-                <AddVocabularyPopup
-                    chapterId={chapterId}
-                    onClose={togglePopup}
-                    onVocabularyAdded={() => window.location.reload()} // Reload to reflect changes
-
-                />
-            )}
-        </div>
-    );
+      {/* Add Vocabulary Popup */}
+      {chapterId && showAddVocabPopup && (
+        <AddVocabularyPopup
+          chapterId={chapterId}
+          onClose={togglePopup}
+          onVocabularyAdded={() => window.location.reload()} // Reload to reflect changes
+        />
+      )}
+    </div>
+  );
 };
 
 export default ChapterDetailsPage;
